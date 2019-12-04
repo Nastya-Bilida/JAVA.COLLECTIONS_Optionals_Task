79c5e96f7437622109b6931fbf79453fb53bfb0d
Nastya Bilida <abilida20@gmail.com>
2019-12-05 01:22:40 +0300

    fixed minor bugs

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,14 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-//5.Не используя вспомогательных объектов, переставить отрицательные элементы данного списка в конец, а положительные — в начало списка.
 public class Main {
     static List<Integer> list = new ArrayList<Integer>();
 
-    static void addNumbers(int quantity){
+    static void addNumbers(int quantityNumbers) {
         Random random = new Random();
-        for (int i = 0; i<quantity; i++ ) {
-            int randomNumber = random.nextInt(40+20)-20;
+        for (int i = 0; i < quantityNumbers; i++) {
+            int randomNumber = random.nextInt(40 + 20) - 10;
             list.add(randomNumber);
         }
         System.out.println(list);
@@ -19,16 +18,12 @@ public class Main {
 
     public static void main(String[] args) {
         addNumbers(5);
-        for (int i = 0; i < list.size(); i++)
-        {
-            if(list.get(i) < 0)
-            {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) < 0) {
                 list.add(list.get(i));
                 list.remove(list.get(i));
             }
         }
-
         System.out.println(list);
-
     }
 }
